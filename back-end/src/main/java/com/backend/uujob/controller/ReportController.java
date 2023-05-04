6d303de495ab2a62827ba6b5bd53f4b90b1ef353
Muzukizh <106598067+Muzukizh@users.noreply.github.com>
2023-05-04 15:43:45 +0800

    add report

---
+++ back-end/src/main/java/com/backend/uujob/controller/ReportController.java
@@ -0,0 +1,40 @@
+package com.backend.uujob.controller;
+
+import com.backend.uujob.entity.Report;
+import com.backend.uujob.enums.ReportStatusEnum;
+import com.backend.uujob.enums.ReportTypeEnum;
+import com.backend.uujob.result.Result;
+import com.backend.uujob.service.IReportService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+    @Resource
+    private IReportService reportService;
+
+    @PostMapping("/jobs")
+    public Result reportJob(@RequestBody Report report){
+        report.setType(ReportTypeEnum.REPORT_TYPE_JOB.ordinal());
+        report.setStatus(ReportStatusEnum.REPORT_STATUS_PENDING.ordinal());
+        reportService.save(report);
+        return Result.success(report);
+    }
+
+    @PostMapping("/posts")
+    public Result reportPost(@RequestBody Report report){
+        report.setType(ReportTypeEnum.REPORT_TYPE_POST.ordinal());
+        report.setStatus(ReportStatusEnum.REPORT_STATUS_PENDING.ordinal());
+        reportService.save(report);
+        return Result.success(report);
+    }
+
+    @PostMapping("/comments")
+    public Result reportComment(@RequestBody Report report){
+        report.setType(ReportTypeEnum.REPORT_TYPE_COMMENT.ordinal());
+        report.setStatus(ReportStatusEnum.REPORT_STATUS_PENDING.ordinal());
+        reportService.save(report);
+        return Result.success(report);
+    }
+}
---
+++ back-end/src/main/java/com/backend/uujob/service/IReportService.java
@@ -0,0 +1,7 @@
+package com.backend.uujob.service;
+
+import com.backend.uujob.entity.Report;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IReportService extends IService<Report> {
+}
