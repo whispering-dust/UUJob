974628ebfc6e1ecffabd6c7d3cf64979388d0b2e
mapleleaf <1029529099@qq.com>
2023-05-06 14:38:55 +0800

    美观一点点

--- front-end/src/view/Home.vue
+++ front-end/src/view/Home.vue
@@ -6,12 +6,15 @@
         <div class="container mt-5" style="height: 70px;width:70%">
 
           <!-- 搜索栏 -->
-          <div class="card row" style="justify-content: center;height: 100%; background-color:#000000;">
-            <el-input round v-model="input" placeholder="Please input" clearable  style="height:100% ;  width:85%;margin:5px;">
+          <div class="card row" style="justify-content: center;height: 80%; background-color:#000000;">
+            <el-input round v-model="input" placeholder="Please input" clearable
+              style="height:100% ;  width:85%;margin:2px;">
             </el-input>
-            <el-link  @click="searchSubmit" :underline="false" style="height: 100%;margin-right:20px;"><h3 class="text-white">搜索</h3></el-link>
+            <el-link @click="searchSubmit" :underline="false" style="height: 100%;margin-right:20px;">
+              <h3 class="text-white">搜索</h3>
+            </el-link>
           </div>
-       
+
         </div>
 
         <div class="container hot-list">
@@ -21,48 +24,46 @@
       </div>
       <div class="bkmain bg-dark">
         <div class="container main">
-          <div class="row">   
-            <div class="col-md-8" >
-              <section class="container" >             
+          <div class="row">
+            <div class="col-md-8">
+              <section class="container">
                 <RecruitArea></RecruitArea>
               </section>
-              
+
             </div>
             <div class="col-md-4">
-              <FunctionArea></FunctionArea>  
+              <FunctionArea></FunctionArea>
             </div>
           </div>
-        </div>        
+        </div>
       </div>
     </div>
-    
-  </div> 
-  
 
+  </div>
 </template>
 
 <script>
-import  RecruitArea   from "@/components/home/RecruitArea.vue";
+import RecruitArea from "@/components/home/RecruitArea.vue";
 import FunctionArea from "@/components/home/FunctionArea.vue";
 import {
   Search,
 } from '@element-plus/icons-vue'
 
-export default{
-  name:"Home",
-  components:{
+export default {
+  name: "Home",
+  components: {
     RecruitArea,
     FunctionArea,
   },
-  data(){
+  data() {
     return {
       Search,
-      input:'',
-      hotList: ['算法', '数据分析师','建筑设计师'],
+      input: '',
+      hotList: ['算法', '数据分析师', '建筑设计师'],
     }
   },
   methods: {
-    searchSubmit(){
+    searchSubmit() {
       alert('Search')
     }
   },
@@ -70,36 +71,39 @@ export default{
 </script>
 
 <style scoped>
-.bkmain{
+.bkmain {
   margin: 0px;
   width: 100%;
   height: 100%;
   min-height: 950px;
   border-radius: 75px 75px 0px 0px;
 }
-.home{
-    width: 100%;
-    height: 100%;
-    min-height:1080px;
-    background-image: url('../../public/assets/images/backgrounds/img-2.jpg');
+
+.home {
+  width: 100%;
+  height: 100%;
+  min-height: 1080px;
+  background-image: url('../../public/assets/images/backgrounds/img-2.jpg');
 }
-.backmask{
+
+.backmask {
   background-color: rgba(255, 255, 255, 0.3);
-  min-height:1080px;
+  min-height: 1080px;
   backdrop-filter: blur(20px);
 }
-.main{
+
+.main {
   width: 100%;
   height: 100%;
 }
 
-.recruit{
+.recruit {
   float: left;
   position: relative;
   left: 20%;
 }
 
-.function{
+.function {
   float: right;
   position: relative;
   right: 8%;
@@ -107,36 +111,39 @@ export default{
 
 
 .el-row {
-    margin-bottom: 20px;
-  }
-  .el-row:last-child {
-    margin-bottom: 0;
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .hot-list {
-    display: flex;
-    flex-wrap: wrap;
-    margin: 10px;
-    padding-left: 150px;
-  }
-  
-  .hot-item {
-    display: inline-block;
-    margin-right: 10px;
-    margin-bottom: 10px;
-    padding: 6px ;
-    padding-bottom: 0px;
-    border: 0px;
-    border-radius: 4px;
-    cursor: pointer;
-    color: black;
-    user-select: none;
-  }
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.hot-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px;
+  padding-left: 150px;
+}
+
+.hot-item {
+  display: inline-block;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  padding: 6px;
+  padding-bottom: 0px;
+  border: 0px;
+  border-radius: 4px;
+  cursor: pointer;
+  color: black;
+  user-select: none;
+}
 </style>
