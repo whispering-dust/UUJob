a2cfe9a07a412b3864c67734d1f90d8e5dca3904
mapleleaf <1029529099@qq.com>
2023-05-09 19:18:36 +0800

    小bug修复

--- front-end/src/components/home/FunctionArea.vue
+++ front-end/src/components/home/FunctionArea.vue
@@ -1,37 +1,36 @@
 <template>
-    <el-card class="box-card2">
-      <template #header>
-        <div class="card-header">
-          <span style="font-size: large;">功能面板</span>
-        </div>
-      </template>
-      <el-button v-if="store.state.role==1" color="black" @click="postJob" :icon="DocumentAdd" plain>
-        发布岗位
-      </el-button>
-    </el-card>
+  <el-card class="box-card2">
+    <template #header>
+      <div class="card-header">
+        <span style="font-size: large;">功能面板</span>
+      </div>
+    </template>
+    <el-button v-if="store.state.role == 1" color="black" @click="postJob" :icon="DocumentAdd" plain>
+      发布岗位
+    </el-button>
+  </el-card>
 
-    <el-dialog  top="0vh" v-model="dialogFormVisible" title="请填写表单信息" draggable lock-scroll=false>
-      <el-scrollbar >
-        <RecruitPost></RecruitPost>
-      </el-scrollbar>
-    </el-dialog>
-
-  </template>
+  <el-dialog top="0vh" v-model="dialogFormVisible" title="请填写表单信息" draggable :lock-scroll=false>
+    <el-scrollbar>
+      <RecruitPost></RecruitPost>
+    </el-scrollbar>
+  </el-dialog>
+</template>
   
 <script>
-import {reactive, ref} from 'vue';
+import { reactive, ref } from 'vue';
 import RecruitPost from "@/components/home/RecruitPost.vue";
-import{useStore} from 'vuex';
+import { useStore } from 'vuex';
 import {
   DocumentAdd
 } from '@element-plus/icons-vue'
-export default{
-  components:{
+export default {
+  components: {
     RecruitPost,
   },
-  data(){
+  data() {
     const store = useStore();
-    const dialogFormVisible  =ref(false);
+    const dialogFormVisible = ref(false);
 
     return {
       dialogFormVisible,
@@ -40,41 +39,41 @@ export default{
     }
   },
   methods: {
-    postJob(){
+    postJob() {
 
-      if(this.store.state.role != 1){
+      if (this.store.state.role != 1) {
         alert('你还没有认证招聘者身份，没有权限')
-      }else{
+      } else {
         this.dialogFormVisible = true
       }
-      
+
     }
   },
 }
 
 </script>
-  <style>
-  .card-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0px;
-    border-bottom: 0px;
-    padding: 0px;
-    width: 100%;
-  }
-  
-  .text {
-    font-size: 14px;
-  }
-  
-  .item {
-    margin-bottom: 18px;
-  }
-  
-  .box-card2 {
-    margin-top: 50px;
-    width: 100%;
-  }
-  </style>
+<style>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px;
+  border-bottom: 0px;
+  padding: 0px;
+  width: 100%;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card2 {
+  margin-top: 50px;
+  width: 100%;
+}
+</style>
   
